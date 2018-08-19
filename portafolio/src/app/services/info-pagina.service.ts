59af6c2430c80e6766b69acf7f6200aa83e0668b
Tux <realsteel.gnu@gmail.com>
2018-08-18 19:09:42 -0500

    Fin de la seccion 5 - Firebase basico

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -10,14 +10,30 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any;
+
   constructor(private http: HttpClient) {
     // console.log('Servicio de info pagina listo');
+    this.cargarInfo();
+    this.cargarEquipo();
+   }
+
+   private cargarInfo() {
+     // leer el archivo json
+     this.http.get('assets/data/data_pagina.json')
+     .subscribe( (resp: InfoPagina) => {
+       this.cargada = true;
+       this.info = resp;
+      //  console.log(resp);
+     });
+   }
+
+   private cargarEquipo() {
     // leer el archivo json
-    this.http.get('assets/data/data_pagina.json')
-      .subscribe( (resp: InfoPagina) => {
-        this.cargada = true;
-        this.info = resp;
-        console.log(resp);
-      });
+    this.http.get('https://angular-html-b833e.firebaseio.com/equipo.json')
+    .subscribe( (resp: any) => {
+      this.equipo = resp;
+      console.log(resp);
+    });
    }
 }
