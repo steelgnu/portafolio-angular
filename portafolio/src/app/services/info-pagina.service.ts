75e0ad94ef8206e8567177097a897a89a7c36eaa
Tux <realsteel.gnu@gmail.com>
2018-08-18 23:43:36 -0500

    Fin de la seccion 6

--- portafolio/src/app/services/info-pagina.service.ts
+++ portafolio/src/app/services/info-pagina.service.ts
@@ -33,7 +33,7 @@ export class InfoPaginaService {
     this.http.get('https://angular-html-b833e.firebaseio.com/equipo.json')
     .subscribe( (resp: any) => {
       this.equipo = resp;
-      console.log(resp);
+      // console.log(resp);
     });
    }
 }
---
+++ portafolio/src/app/services/productos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ProductoInterface } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: ProductoInterface[] = [];
+
+  constructor( private http: HttpClient) {
+    this.cargarProductos();
+  }
+
+  private cargarProductos() {
+    this.http.get('https://angular-html-b833e.firebaseio.com/productos_idx.json')
+    .subscribe( (resp: ProductoInterface[]) => {
+      console.log(resp);
+      this.productos = resp;
+      this.cargando = false;
+      // setTimeout(() => {
+      //   this.cargando = false;
+      // }, 2000);
+    });
+  }
+}
