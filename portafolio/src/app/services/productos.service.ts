14f0766e2473e9222ea01e99f60f152cd18b3c9c
Tux <realsteel.gnu@gmail.com>
2018-08-21 23:41:51 -0500

    Fin de la seccion 7

--- portafolio/src/app/services/productos.service.ts
+++ portafolio/src/app/services/productos.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProductoInterface } from '../interfaces/producto.interface';
+import { reject } from 'q';
 
 @Injectable({
   providedIn: 'root'
@@ -9,25 +10,59 @@ export class ProductosService {
 
   cargando = true;
   productos: ProductoInterface[] = [];
+  productosFiltrado: ProductoInterface[] = [];
 
-  constructor( private http: HttpClient) {
+  constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-b833e.firebaseio.com/productos_idx.json')
-    .subscribe( (resp: ProductoInterface[]) => {
-      console.log(resp);
-      this.productos = resp;
-      this.cargando = false;
-      // setTimeout(() => {
-      //   this.cargando = false;
-      // }, 2000);
+
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-b833e.firebaseio.com/productos_idx.json')
+        .subscribe((resp: ProductoInterface[]) => {
+          // console.log(resp);
+          this.productos = resp;
+          this.cargando = false;
+          resolve();
+        });
     });
   }
 
   getProducto(id: string) {
     return this.http.get(`https://angular-html-b833e.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string) {
+    if (this.productos.length === 0) {
+      // cargar productos
+      this.cargarProductos().then(() => {
+        // ejecutar despues de tener los productos
+        // aplicar el filtro
+        this.filtrarProductos(termino);
+      });
+    } else {
+      // aplicar el filtro
+      this.filtrarProductos(termino);
+    }
+    // this.productosFiltrado = this.productos.filter(producto => {
+    //   return true;
+    // });
+
+    // console.log(this.productosFiltrado);
+  }
+  // metodo para el filtrado
+  private filtrarProductos(termino) {
+    // console.log(this.productos);
+    this.productosFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+    this.productos.forEach(prod => {
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      // buscar las categorias que coincidan con el termino de busqueda
+      if (prod.categoria.indexOf(termino) >= 0 || tituloLower.indexOf(termino) >= 0 ) {
+        this.productosFiltrado.push(prod);
+      }
+    });
   }
 
 }
